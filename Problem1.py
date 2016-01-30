9a622ce860c3832994f984a39b1465213983a24e
Charlotte Aten <caten2@u.rochester.edu>
2016-01-29 22:23:22 -0500

    Worked on documentation Started problem 3

--- Problem1.py
+++ Problem1.py
@@ -1,50 +1,110 @@
-# Project Euler problem 1
-# Solution by Charlotte Aten (caten2@u.rochester.edu)
-# Problem statement:
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+Project Euler problem 1
 
-class Multiples():
-    """The set of all multiples of the elements in lis strictly less than maximum."""
+Solution by Charlotte Aten (caten2@u.rochester.edu) 2015
 
-    def __init__(self, lis, maximum):
-        self.multiplesOf = lis # Give the list of numbers of which we will look at multiples.
-        self.maximum = maximum # Give the maximum number to be considered.
+Problem statement:
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
-    # Create a generator for all numbers below self.maximum which are multiples of
-    # elements of self.lis.
-    def Elements(self):
+class MultiplesUpTo():
+    """
+    A collection of multiples of some natural numbers.
+
+    Attributes:
+        multiples_of (set): Multiples of the natural numbers in this set are considered.
+        list_multiples_of (lis): Sorted list of the natural numbers in the set `multiples_of`.
+        maximum (int): No multiples of elements of `multiples_of` greater than or equal to `maximum` are considered.
+    """
+
+    def __init__(self, nums, maximum):
+        """
+        Args:
+            nums (list): A list of natural numbers.
+            maximum (int): The maximum number up to which to consider multiples of the integers in `nums`.
+        """
+
+        assert len(nums) > 0
+        self.multiples_of = frozenset(nums)
+        self.list_multiples_of = sorted(list(self.multiples_of))
+        self.maximum = maximum
+
+    def __repr__(self):
+        """
+        Examples:
+            >>> M = MultiplesUpTo([3,5],10); M
+            'Multiples of 3 and 5 strictly less than 10.'
+            >>> N = MultiplesUpTo([3,5,7,5,3,7],50); N
+            'Multiples of 3, 5, and 7 strictly less than 50.'
+        """
+
+        if len(self.multiples_of) == 1:
+            return 'Multiples of {} strictly less than {}.'.format(self.list_multiples_of[0],self.maximum)
+        if len(self.multiples_of) == 2:
+            lis = self.list_multiples_of
+            return 'Multiples of {} and {} strictly less than {}.'.format(lis[0],lis[1],self.maximum)
+        if len(self.multiples_of) > 2:
+            lis = self.list_multiples_of
+            parsed_lis = ', '.join([str(num) for num in lis[:-1]])+', '
+            return 'Multiples of ' + parsed_lis + 'and {} strictly less than {}.'.format(lis[-1],self.maximum)
+
+    def elements(self):
+        """
+        Generate numbers below `maximum` which are multiples of elements of `multiples_of`.
+
+        Yields:
+            int: The next number which is a multiple of an element of `nums`.
+        """
+
         for i in range(1,self.maximum):
-            for n in self.multiplesOf:
-                # Check if i is a multiple of n by noting that n divides i iff i is
-                # congruent to 0 modulo n.
+            for n in self.multiples_of:
+                # Check whether i is a multiple of n by noting that n divides i iff i is congruent to 0 modulo n.
                 if i % n == 0:
                     yield i
 
-    # Give a list of the numbers yielded by the above generator.
-    def ListElements(self):
-        return list(self.Elements())
+    def list_elements(self):
+        """
+        List all the numbers yielded by the generator ``elements``.
+        
+        Returns:
+            list: All numbers yielded by ``elements`` in numerical order.
+        """
 
-    # Sum all numbers yielded by the above generator.
-    def Total(self):
-        return sum(self.Elements())
+        return list(self.elements())
 
-print("Create an object for the multiples of 3 and 5 below 10.")
-M = Multiples([3,5],10)
+    def total(self):
+        """
+        Sum all numbers yielded by ``elements``.
+        
+        Returns:
+            int: The sum of all natural multiples of elements of `multiples_of` less than `maximum`.
+        """
 
-print("List all of the multiples of 3 and 5 below 10.")
-print(M.ListElements())
+        return sum(self.elements())
 
-print("Give the sum of those values.")
-print(M.Total())
+print("Create an object for the multiples of 3 and 5 below 10 and have it give its name.")
+M = MultiplesUpTo([3,5],10)
+print(M)
 print("")
 
-print("Create an object for the multiples of 3 and 5 below 1000.")
-N = Multiples([3,5],1000)
-
-print("List all of the multiples of 3 and 5 below 1000.")
-print(N.ListElements())
+print("List all of the multiples of 3 and 5 below 10.")
+print(M.list_elements())
+print("")
 
 print("Give the sum of those values.")
-print(N.Total())
+print(M.total())
+print("")
+
+print("Create an object for the multiples of 3 and 5 below 1000 and have it give its name.")
+N = MultiplesUpTo([3,5],1000)
+print(N)
+print("")
+
+print("List all of the multiples of 3 and 5 below 1000.")
+print(N.list_elements())
+print("")
+
+print("Give the sum of those values.")
+print(N.total())
--- Problem3.py
+++ Problem3.py
@@ -1,14 +1,18 @@
-# Project Euler problem 3
-# Solution by Charlotte Aten (caten2@u.rochester.edu)
-# Problem statement:
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143?
+"""
+Project Euler problem 3
+
+Solution by Charlotte Aten (caten2@u.rochester.edu) 2015
+
+Problem statement:
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143?
+"""
 
 class PrimeFactors():
     """
-    The collection of prime factors of a positive integer i.
+    The collection of natural prime factors of a specified integer.
     """
-    
+
     def __init__(self,i):
         self.integer = i # Return the integer in question.
         self.primeFactors = list(self.PrimeFactors()) # Return a list of the prime factors of i.
